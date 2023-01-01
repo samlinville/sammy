81072bcdfaabae0db3113faf4d326ed40bf914df
Sam Linville <samlinville@protonmail.com>
2023-01-01 11:49:49 -0500

    quality of life updates

--- src/components/MusicPill.svelte
+++ src/components/MusicPill.svelte
@@ -16,26 +16,32 @@
 </script>
 
 
-<div class="hidden sm:flex items-center justify-center musicpill transition-colors duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
+
     {#await promise}
+    <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
         <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
         <div class="ml-4">
-            <p class="musictitle text-xs text-zinc-200 transition-colors duration-300 leading-0 mb-0">Loading...</p>
+            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Loading...</p>
         </div>
+    </div>
     {:then song}
+    <div class="hidden sm:visible sm:flex items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
         <img alt="An 8-bit pixel art of a compact disc rotating" class="rotate h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
         <div class="ml-4">
-            <p class="musictitle text-xs text-zinc-200 transition-colors duration-300 leading-0 mb-0">{song.title}</p>
-            <p class="musicartist text-xs text-zinc-400 transition-colors duration-300 leading-0 mb-0">{song.artist}</p>
+            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">{song.title}</p>
+            <p class="font-mono musicartist text-xs text-zinc-400 transition-all duration-300 leading-0 mb-0">{song.artist}</p>
         </div>
+    </div>
     {:catch error}
+    <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
         <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
         <div class="ml-4">
-            <p class="musictitle text-xs text-zinc-200 transition-colors duration-300 leading-0 mb-0">Nothing playing</p>
+            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Nothing playing</p>
         </div>
+    </div>
     {/await}        
     
-</div>
+
 
 <style>
     .musicpill:hover img {
