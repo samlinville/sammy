367a38ee5991d3f59788ee981e14986b22f8a6fa
Sam Linville <samlinville@protonmail.com>
2023-01-05 20:05:53 -0500

    fix font weight bug

--- src/config.ts
+++ src/config.ts
@@ -1,45 +1,46 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sam Linville';
-export const SITE_SUBTITLE = 'Build cool stuff.';
-export const SITE_DESCRIPTION = 'Product Manager';
+export const SITE_TITLE = "Sam Linville";
+export const SITE_SUBTITLE = "Build cool stuff.";
+export const SITE_DESCRIPTION =
+  "Design + code. On a journey to build good things. Interested in cloud infrastructure, security, and developer experience. Product Manager for HashiCorp Vault.";
 export const SITE_NAVIGATION = [
-    {
-        title: 'Now',
-        link: '/now',
-        blank: false
-    },
-    {
-        title: 'Blog',
-        link: '/blog',
-        blank: false
-    },
-    {
-        title: 'Projects',
-        link: '/projects',
-        blank: false
-    }
+  {
+    title: "Now",
+    link: "/now",
+    blank: false,
+  },
+  {
+    title: "Blog",
+    link: "/blog",
+    blank: false,
+  },
+  {
+    title: "Projects",
+    link: "/projects",
+    blank: false,
+  },
 ];
 export const SOCIAL_LINKS = {
-    twitter: {
-        title: 'Twitter',
-        link: 'https://twitter.com/samlinville',
-        handle: '@samlinville',
-    },
-    mastodon: {
-        title: 'Hachyderm.io',
-        link: 'https://hachyderm.io/@thrillifying',
-        handle: '@thrillifying',
-    },
-    github: {
-        title: 'GitHub',
-        link: 'https://github.com/samlinville',
-        handle: '/samlinville',
-    },
-    linkedin: {
-        title: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/samlinville/',
-        handle: '/in/samlinville'
-    }
-}
+  twitter: {
+    title: "Twitter",
+    link: "https://twitter.com/samlinville",
+    handle: "@samlinville",
+  },
+  mastodon: {
+    title: "Hachyderm.io",
+    link: "https://hachyderm.io/@thrillifying",
+    handle: "@thrillifying",
+  },
+  github: {
+    title: "GitHub",
+    link: "https://github.com/samlinville",
+    handle: "/samlinville",
+  },
+  linkedin: {
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/samlinville/",
+    handle: "/in/samlinville",
+  },
+};
