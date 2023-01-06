4f8d8f969ecd3f0bf5c371542bffbfbc6ad8aeaf
Sam Linville <samlinville@protonmail.com>
2023-01-06 06:47:30 -0500

    remove disc opacity

--- src/components/MusicPill.svelte
+++ src/components/MusicPill.svelte
@@ -19,14 +19,14 @@
 
     {#await promise}
     <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
+        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] duration-300" src={Disc}>
         <div class="ml-4">
             <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Loading...</p>
         </div>
     </div>
     {:then song}
     <div class="hidden sm:visible sm:flex items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="rotate h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
+        <img alt="An 8-bit pixel art of a compact disc rotating" class="rotate h-[35px] duration-300" src={Disc}>
         <div class="ml-4">
             <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">{song.title}</p>
             <p class="font-mono musicartist text-xs text-zinc-400 transition-all duration-300 leading-0 mb-0">{song.artist}</p>
@@ -34,7 +34,7 @@
     </div>
     {:catch error}
     <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] opacity-75 transition-opacity duration-300" src={Disc}>
+        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] duration-300" src={Disc}>
         <div class="ml-4">
             <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Nothing playing</p>
         </div>
@@ -44,9 +44,6 @@
 
 
 <style>
-    .musicpill:hover img {
-        @apply opacity-100;
-    }
 
     .musicpill:hover .musictitle {
         @apply text-zinc-200;
