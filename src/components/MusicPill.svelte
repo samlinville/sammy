1c095db6d5b5f7fef0af511dc2ef02a4b6cc721a
Sam Linville <samlinville@protonmail.com>
2023-01-07 12:22:41 -0500

    add reloading to music player

--- src/components/MusicPill.svelte
+++ src/components/MusicPill.svelte
@@ -1,70 +1,90 @@
 <script>
-	import Disc from "../assets/images/disc.png"
+  import { fade } from "svelte/transition";
+  import Disc from "../assets/images/disc.png";
 
-    let promise = getCurrentSong();
+  let visible = false;
+  let song = null;
+  let artist = null;
+  let songPlaying = false;
 
-	async function getCurrentSong() {
-		const res = await fetch(`https://sammy-service.fly.dev/spotify/current`);
-		const msg = await res.json();
-		const data = msg.message
-		if (res.ok) {
-			return data;
-		} else {
-			throw new Error(data);
-		}
-	}
+  //fetch the initial song status
+  getSong();
+
+  //refresh the song status every 20 seconds
+  setInterval(() => {
+    getSong();
+  }, 20000);
+
+  function getSong() {
+    getCurrentSong().then((data) => {
+      try {
+        song = data.title;
+        artist = data.artist;
+        songPlaying = data.isPlaying;
+        visible = true;
+      } catch {
+        visible: false;
+      }
+    });
+  }
+
+  async function getCurrentSong() {
+    const res = await fetch(`https://sammy-service.fly.dev/spotify/current`);
+    const msg = await res.json();
+    const data = msg.message;
+    if (res.ok) {
+      return data;
+    } else {
+      throw new Error(data);
+    }
+  }
 </script>
 
-
-
-    {#await promise}
-    <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] duration-300" src={Disc}>
-        <div class="ml-4">
-            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Loading...</p>
-        </div>
+{#if visible}
+  <div
+    in:fade
+    out:fade
+    class="hidden sm:visible sm:flex items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900"
+  >
+    <img
+      alt="An 8-bit pixel art of a compact disc rotating"
+      class="{songPlaying ? 'rotate' : ''} h-[35px] duration-300"
+      src={Disc}
+    />
+    <div class="ml-4">
+      <p
+        class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0"
+      >
+        {song}
+      </p>
+      <p
+        class="font-mono musicartist text-xs text-zinc-400 transition-all duration-300 leading-0 mb-0"
+      >
+        {artist}
+      </p>
     </div>
-    {:then song}
-    <div class="hidden sm:visible sm:flex items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="rotate h-[35px] duration-300" src={Disc}>
-        <div class="ml-4">
-            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">{song.title}</p>
-            <p class="font-mono musicartist text-xs text-zinc-400 transition-all duration-300 leading-0 mb-0">{song.artist}</p>
-        </div>
-    </div>
-    {:catch error}
-    <div class="hidden sm:flex sm:invisible items-center justify-center musicpill transition-all duration-300 border rounded-full border-zinc-800 hover:border-zinc-600 pl-1 py-1 pr-6 bg-zinc-900">
-        <img alt="An 8-bit pixel art of a compact disc rotating" class="h-[35px] duration-300" src={Disc}>
-        <div class="ml-4">
-            <p class="font-mono musictitle text-xs text-zinc-200 transition-all duration-300 leading-0 mb-0">Nothing playing</p>
-        </div>
-    </div>
-    {/await}        
-    
-
+  </div>
+{/if}
 
 <style>
+  .musicpill:hover .musictitle {
+    @apply text-zinc-200;
+  }
 
-    .musicpill:hover .musictitle {
-        @apply text-zinc-200;
-    }
+  .musicpill:hover .musicartist {
+    @apply text-zinc-400;
+  }
 
-    .musicpill:hover .musicartist {
-        @apply text-zinc-400;
-    }
+  .rotate {
+    animation: rotation 1s infinite linear;
+  }
 
-    .rotate {
-        animation: rotation 1s infinite linear;
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
     }
-
-    @keyframes rotation {
-        from {
-            transform: rotate(0deg);
-        }
-        to {
-            transform: rotate(359deg);
-        }
+    to {
+      transform: rotate(359deg);
     }
+  }
 </style>
-      
-
