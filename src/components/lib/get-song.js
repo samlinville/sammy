c31d4687b9b3a5c14b457967c4ff5e6c04672afe
Sam Linville <samlinville@protonmail.com>
2023-01-27 16:11:22 -0500

    remove weird dupe request

--- src/components/lib/get-song.js
+++ src/components/lib/get-song.js
@@ -3,6 +3,7 @@ async function getCurrentSong() {
     method: 'GET',
     withCredentials: true,
     crossorigin: true,
+    mode: 'no-cors'
   });
   const msg = await res.json();
   const data = msg.message;
