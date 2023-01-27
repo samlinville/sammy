8a06c1ee6c2f5e42e4123aa37ab8e3fb5050419a
Sam Linville <samlinville@protonmail.com>
2023-01-27 16:18:51 -0500

    Revert "change spoti-api url"
    This reverts commit 403cc0a89609f132c1c7ff77f6ddf8d0a95883c5.

--- src/components/lib/get-song.js
+++ src/components/lib/get-song.js
@@ -1,8 +1,5 @@
 async function getCurrentSong() {
   const res = await fetch('https://sammy-services.phoenix-cobra.ts.net/spotify/current', {
-    method: 'GET',
-    withCredentials: true,
-    crossorigin: true,
   });
   const msg = await res.json();
   const data = msg.message;
