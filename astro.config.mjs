9c52cad23ec3b088e77b22a310cf81ba0ddb46c9
Sam Linville <samlinville@protonmail.com>
2023-05-26 20:32:10 -0400

    update to 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-// import torchlight from 'remark-torchlight';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// import customMono from './src/assets/monokai.json';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
-  markdown: {
-    extendDefaultPlugins: true
-    // remarkPlugins: [torchlight]
-  },
+  site: "https://samlinville.com/",
+  integrations: [tailwind(), mdx(), sitemap(), svelte()]
 });
